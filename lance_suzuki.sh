2e709ae71d786c8aa67b31b2c8a3933c01455801
Nazif Can Tamer <nazifcan.tamer@upf.edu>
2022-11-22 09:49:13 +0100

    Update lance_suzuki.sh

--- lance_suzuki.sh
+++ lance_suzuki.sh
@@ -20,39 +20,39 @@ conda activate supervised
 ########
 # Kohler
 # 25 Romantic Etudes Op 66
-yt-dlp --yes-playlist --playlist-items 1,2,4-20,22 https://youtube.com/playlist?list=PLsrOzJDmkmQ28jjKCy8O8D3IkxJt4UG9H -x --audio-format mp3 --audio-quality 0   -o "flute/LanceSuzuki_KohlerOp66Romantic_%(playlist_index)03d_%(title)s_[%(id)s]_0000_%(duration)04d.%(ext)s"
+yt-dlp --yes-playlist --playlist-items 1,2,4-20,22 https://youtube.com/playlist?list=PLsrOzJDmkmQ28jjKCy8O8D3IkxJt4UG9H -x --audio-format mp3 --audio-quality 0  -i -o "flute/LanceSuzuki_KohlerOp66Romantic_%(playlist_index)03d_%(title)s_[%(id)s]_0000_%(duration)04d.%(ext)s"
 
 # Expression Etudes *3
-yt-dlp --yes-playlist https://www.youtube.com/playlist?list=PLsrOzJDmkmQ2xe5YqwJzPTyV-nuAiB4cJ -x --audio-format mp3 --audio-quality 0   -o "flute/LanceSuzuki_KohlerExpression_%(playlist_index)03d_%(title)s_[%(id)s]_0000_%(duration)04d.%(ext)s"
+yt-dlp --yes-playlist https://www.youtube.com/playlist?list=PLsrOzJDmkmQ2xe5YqwJzPTyV-nuAiB4cJ -x --audio-format mp3 --audio-quality 0  -i -o "flute/LanceSuzuki_KohlerExpression_%(playlist_index)03d_%(title)s_[%(id)s]_0000_%(duration)04d.%(ext)s"
 
 # Op 33
-yt-dlp --yes-playlist https://youtube.com/playlist?list=PLsrOzJDmkmQ2WJp0pLq-GZGHpXtfAtsyJ -x --audio-format mp3 --audio-quality 0   -o "flute/LanceSuzuki_KohlerOp33_%(playlist_index)03d_%(title)s_[%(id)s]_0000_%(duration)04d.%(ext)s"
+yt-dlp --yes-playlist https://youtube.com/playlist?list=PLsrOzJDmkmQ2WJp0pLq-GZGHpXtfAtsyJ -x --audio-format mp3 --audio-quality 0 -i  -o "flute/LanceSuzuki_KohlerOp33_%(playlist_index)03d_%(title)s_[%(id)s]_0000_%(duration)04d.%(ext)s"
 
 
 
 ########
 # Drouet
 # 25 etudes *4/25
-yt-dlp --yes-playlist --playlist-items 1,3,4 https://www.youtube.com/playlist?list=PLsrOzJDmkmQ3y-rJhUzvo6Xwpr8WMxzi6-x --audio-format mp3 --audio-quality 0   -o "flute/LanceSuzuki_Drouet25_%(playlist_index)03d_%(title)s_[%(id)s]_0000_%(duration)04d.%(ext)s"
+yt-dlp --yes-playlist --playlist-items 1,3,4 https://www.youtube.com/playlist?list=PLsrOzJDmkmQ3y-rJhUzvo6Xwpr8WMxzi6-x --audio-format mp3 --audio-quality 0  -i -o "flute/LanceSuzuki_Drouet25_%(playlist_index)03d_%(title)s_[%(id)s]_0000_%(duration)04d.%(ext)s"
 
 
 
 ###########
 # Gariboldi 
 # Op 132
-yt-dlp --yes-playlist --playlist-items 2-13,15-20 https://youtube.com/playlist?list=PLsrOzJDmkmQ2wsNrkmmLVnKtcAZlxHK67 --audio-format mp3 --audio-quality 0   -o "flute/LanceSuzuki_GariboldiOp132_%(playlist_index)03d_%(title)s_[%(id)s]_0000_%(duration)04d.%(ext)s"
+yt-dlp --yes-playlist --playlist-items 2-13,15-20 https://youtube.com/playlist?list=PLsrOzJDmkmQ2wsNrkmmLVnKtcAZlxHK67 --audio-format mp3 --audio-quality 0 -i  -o "flute/LanceSuzuki_GariboldiOp132_%(playlist_index)03d_%(title)s_[%(id)s]_0000_%(duration)04d.%(ext)s"
 
 
 ###########
 # Andersen
 # Op 41
-yt-dlp --yes-playlist https://youtube.com/playlist?list=PLsrOzJDmkmQ1udBbsJHbA3o2v0pJBgLg-  --audio-format mp3 --audio-quality 0   -o "flute/LanceSuzuki_AndersenOp41_%(playlist_index)03d_%(title)s_[%(id)s]_0000_%(duration)04d.%(ext)s"
+yt-dlp --yes-playlist https://youtube.com/playlist?list=PLsrOzJDmkmQ1udBbsJHbA3o2v0pJBgLg-  --audio-format mp3 --audio-quality 0 -i  -o "flute/LanceSuzuki_AndersenOp41_%(playlist_index)03d_%(title)s_[%(id)s]_0000_%(duration)04d.%(ext)s"
 
 
 
 ########
 # Donjon
 # 8 etudes de salon
-yt-dlp --yes-playlist https://www.youtube.com/playlist?list=PLsrOzJDmkmQ2Mzb8IXxCUy9bDqwRmU6mB  --audio-format mp3 --audio-quality 0   -o "flute/LanceSuzuki_Donjon8Salon_%(playlist_index)03d_%(title)s_[%(id)s]_0000_%(duration)04d.%(ext)s"
+yt-dlp --yes-playlist https://www.youtube.com/playlist?list=PLsrOzJDmkmQ2Mzb8IXxCUy9bDqwRmU6mB  --audio-format mp3 --audio-quality 0  -i -o "flute/LanceSuzuki_Donjon8Salon_%(playlist_index)03d_%(title)s_[%(id)s]_0000_%(duration)04d.%(ext)s"
 
 

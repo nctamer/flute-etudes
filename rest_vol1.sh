ad12d3fa8b73bf5dda4da303b963ae77c5293676
Nazif Can Tamer <nazifcan.tamer@upf.edu>
2022-11-22 20:22:59 +0100

    Update rest_vol1.sh

--- rest_vol1.sh
+++ rest_vol1.sh
@@ -20,14 +20,14 @@ conda activate supervised
 # Viviana Guzman
 ########
 # Mix Etudes / Donjon, Drouet, Igudesman
-yt-dlp --yes-playlist https://youtube.com/playlist?list=PLXkCr-08yJ-8jJ3sBuIsOnzarfsa08KzO --audio-format mp3 --audio-quality 0 -i  -o "flute/VivianaGuzman_Mix_%(playlist_index)03d_%(title)s_[%(id)s]_0000_%(duration)04d.%(ext)s"
+yt-dlp --yes-playlist https://youtube.com/playlist?list=PLXkCr-08yJ-8jJ3sBuIsOnzarfsa08KzO -x --audio-format mp3 --audio-quality 0 -i  -o "flute/VivianaGuzman_Mix_%(playlist_index)03d_%(title)s_[%(id)s]_0000_%(duration)04d.%(ext)s"
 
 
 ########
 # Amy Porter
 ########
 # KargElert30
-yt-dlp --yes-playlist --playlist-items 5-36 https://www.youtube.com/playlist?list=PLpc7xR4RabCZrDtGlrl3jPWcKyh1MJ4vV --audio-format mp3 --audio-quality 0  -i -o "flute/AmyPorter_Elert30Mix_%(playlist_index)03d_%(title)s_[%(id)s]_0000_%(duration)04d.%(ext)s"
+yt-dlp --yes-playlist --playlist-items 5-36 https://www.youtube.com/playlist?list=PLpc7xR4RabCZrDtGlrl3jPWcKyh1MJ4vV -x --audio-format mp3 --audio-quality 0  -i -o "flute/AmyPorter_Elert30Mix_%(playlist_index)03d_%(title)s_[%(id)s]_0000_%(duration)04d.%(ext)s"
 
 
 
@@ -35,9 +35,9 @@ yt-dlp --yes-playlist --playlist-items 5-36 https://www.youtube.com/playlist?lis
 # Catherine Baker
 ########
 # Drouet 25 *24/25
-yt-dlp --yes-playlist https://youtube.com/playlist?list=PLdKMswDqnn6tK0C5BjnxeJkonZkEDGoOg --audio-format mp3 --audio-quality 0 -i  -o "flute/CatherineBaker_Drouet25_%(playlist_index)03d_%(title)s_[%(id)s]_0000_%(duration)04d.%(ext)s"
+yt-dlp --yes-playlist https://youtube.com/playlist?list=PLdKMswDqnn6tK0C5BjnxeJkonZkEDGoOg -x --audio-format mp3 --audio-quality 0 -i  -o "flute/CatherineBaker_Drouet25_%(playlist_index)03d_%(title)s_[%(id)s]_0000_%(duration)04d.%(ext)s"
 # Donjon Salon
-yt-dlp --yes-playlist https://youtube.com/playlist?list=PLdKMswDqnn6tZ9N1jEL2YNE99WEXBJo66 --audio-format mp3 --audio-quality 0  -i -o "flute/CatherineBaker_Donjon8Salon_%(playlist_index)03d_%(title)s_[%(id)s]_0000_%(duration)04d.%(ext)s"
+yt-dlp --yes-playlist https://youtube.com/playlist?list=PLdKMswDqnn6tZ9N1jEL2YNE99WEXBJo66 -x --audio-format mp3 --audio-quality 0  -i -o "flute/CatherineBaker_Donjon8Salon_%(playlist_index)03d_%(title)s_[%(id)s]_0000_%(duration)04d.%(ext)s"
 
 
 
@@ -47,13 +47,13 @@ yt-dlp --yes-playlist https://youtube.com/playlist?list=PLdKMswDqnn6tZ9N1jEL2YNE
 ######
 # Bona
 # Rhythmical Articulation
-yt-dlp --yes-playlist https://www.youtube.com/playlist?list=PLTfttGatEdgsiw405mcpfg0vCywEN-D-B --audio-format mp3 --audio-quality 0  -i -o "flute/AbigailWalsh_BonaArticulation_%(playlist_index)03d_%(title)s_[%(id)s]_0000_%(duration)04d.%(ext)s"
+yt-dlp --yes-playlist https://www.youtube.com/playlist?list=PLTfttGatEdgsiw405mcpfg0vCywEN-D-B -x --audio-format mp3 --audio-quality 0  -i -o "flute/AbigailWalsh_BonaArticulation_%(playlist_index)03d_%(title)s_[%(id)s]_0000_%(duration)04d.%(ext)s"
 ######
 # Drouet 25 *24/25
-yt-dlp --yes-playlist https://www.youtube.com/playlist?list=PLTfttGatEdgvCGkwwDzy4nzXVUvXUE5q9 --audio-format mp3 --audio-quality 0  -i -o "flute/AbigailWalsh_Drouet25_%(playlist_index)03d_%(title)s_[%(id)s]_0000_%(duration)04d.%(ext)s"
+yt-dlp --yes-playlist https://www.youtube.com/playlist?list=PLTfttGatEdgvCGkwwDzy4nzXVUvXUE5q9 -x --audio-format mp3 --audio-quality 0  -i -o "flute/AbigailWalsh_Drouet25_%(playlist_index)03d_%(title)s_[%(id)s]_0000_%(duration)04d.%(ext)s"
 ######
 # Kohher
 # 25 Romantic Etudes Op 66, ALL 25!!
-yt-dlp --yes-playlist https://youtube.com/playlist?list=PLTfttGatEdgu7v82ecHNlO3pPj4bJzZk7 --audio-format mp3 --audio-quality 0  -i -o "flute/AbigailWalsh_KohlerOp66Romantic_%(playlist_index)03d_%(title)s_[%(id)s]_0000_%(duration)04d.%(ext)s"
+yt-dlp --yes-playlist https://youtube.com/playlist?list=PLTfttGatEdgu7v82ecHNlO3pPj4bJzZk7 -x --audio-format mp3 --audio-quality 0  -i -o "flute/AbigailWalsh_KohlerOp66Romantic_%(playlist_index)03d_%(title)s_[%(id)s]_0000_%(duration)04d.%(ext)s"
 
 

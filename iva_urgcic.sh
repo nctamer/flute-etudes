464dc1ff5a73210d587e6ee6d5ecfd788fd742d1
Nazif Can Tamer <nazifcan.tamer@upf.edu>
2022-11-22 09:49:48 +0100

    Update iva_urgcic.sh

--- iva_urgcic.sh
+++ iva_urgcic.sh
@@ -19,13 +19,13 @@ conda activate supervised
 ########
 # Drouet
 # 25 etudes *20/25
-yt-dlp --yes-playlist https://youtube.com/playlist?list=PLGCSTfKla3Cs7Ro2VgTcD79k0tQP29VEs --audio-format mp3 --audio-quality 0   -o "flute/IvaUrgcic_Drouet25_%(playlist_index)03d_%(title)s_[%(id)s]_0000_%(duration)04d.%(ext)s"
+yt-dlp --yes-playlist https://youtube.com/playlist?list=PLGCSTfKla3Cs7Ro2VgTcD79k0tQP29VEs --audio-format mp3 --audio-quality 0  -i -o "flute/IvaUrgcic_Drouet25_%(playlist_index)03d_%(title)s_[%(id)s]_0000_%(duration)04d.%(ext)s"
 
 
 ########
 # Donjon
 # 8 etudes de salon
-yt-dlp --yes-playlist https://youtube.com/playlist?list=PLGCSTfKla3Cu0bGXuRSl34HvaHrhA4xKu  --audio-format mp3 --audio-quality 0   -o "flute/IvaUrgcic_Donjon8Salon_%(playlist_index)03d_%(title)s_[%(id)s]_0000_%(duration)04d.%(ext)s"
+yt-dlp --yes-playlist https://youtube.com/playlist?list=PLGCSTfKla3Cu0bGXuRSl34HvaHrhA4xKu  --audio-format mp3 --audio-quality 0  -i -o "flute/IvaUrgcic_Donjon8Salon_%(playlist_index)03d_%(title)s_[%(id)s]_0000_%(duration)04d.%(ext)s"
 
 
 
@@ -34,13 +34,13 @@ yt-dlp --yes-playlist https://youtube.com/playlist?list=PLGCSTfKla3Cu0bGXuRSl34H
 ########
 # Kohler
 # 25 Romantic Etudes Op 66, 5/25
-yt-dlp --yes-playlist https://youtube.com/playlist?list=PLGCSTfKla3CuMl3g5SVnM4wLEjj_6Zv86 -x --audio-format mp3 --audio-quality 0   -o "flute/IvaUrgcic_KohlerOp66Romantic_%(playlist_index)03d_%(title)s_[%(id)s]_0000_%(duration)04d.%(ext)s"
+yt-dlp --yes-playlist https://youtube.com/playlist?list=PLGCSTfKla3CuMl3g5SVnM4wLEjj_6Zv86 -x --audio-format mp3 --audio-quality 0 -i  -o "flute/IvaUrgcic_KohlerOp66Romantic_%(playlist_index)03d_%(title)s_[%(id)s]_0000_%(duration)04d.%(ext)s"
 
 # Expression Etudes , all 8
-yt-dlp --yes-playlist https://youtube.com/playlist?list=PLGCSTfKla3CtEMSC8YWuaAEfamfA7fsi3 -x --audio-format mp3 --audio-quality 0   -o "flute/IvaUrgcic_KohlerExpression_%(playlist_index)03d_%(title)s_[%(id)s]_0000_%(duration)04d.%(ext)s"
+yt-dlp --yes-playlist https://youtube.com/playlist?list=PLGCSTfKla3CtEMSC8YWuaAEfamfA7fsi3 -x --audio-format mp3 --audio-quality 0 -i  -o "flute/IvaUrgcic_KohlerExpression_%(playlist_index)03d_%(title)s_[%(id)s]_0000_%(duration)04d.%(ext)s"
 
 # Op 33. only 3
-yt-dlp --yes-playlist https://www.youtube.com/playlist?list=PLGCSTfKla3CuH7qjjvkQp0sapTRwylTde -x --audio-format mp3 --audio-quality 0   -o "flute/IvaUrgcic_KohlerOp33_%(playlist_index)03d_%(title)s_[%(id)s]_0000_%(duration)04d.%(ext)s"
+yt-dlp --yes-playlist https://www.youtube.com/playlist?list=PLGCSTfKla3CuH7qjjvkQp0sapTRwylTde -x --audio-format mp3 --audio-quality 0 -i  -o "flute/IvaUrgcic_KohlerOp33_%(playlist_index)03d_%(title)s_[%(id)s]_0000_%(duration)04d.%(ext)s"
 
 
 
@@ -50,6 +50,6 @@ yt-dlp --yes-playlist https://www.youtube.com/playlist?list=PLGCSTfKla3CuH7qjjvk
 ###########
 # Gariboldi 
 # Op 132, only 4
-yt-dlp --yes-playlist https://www.youtube.com/playlist?list=PLGCSTfKla3Ctgh5fswREiG49UyXqziNvY --audio-format mp3 --audio-quality 0   -o "flute/IvaUrgcic_GariboldiOp132_%(playlist_index)03d_%(title)s_[%(id)s]_0000_%(duration)04d.%(ext)s"
+yt-dlp --yes-playlist https://www.youtube.com/playlist?list=PLGCSTfKla3Ctgh5fswREiG49UyXqziNvY --audio-format mp3 --audio-quality 0 -i  -o "flute/IvaUrgcic_GariboldiOp132_%(playlist_index)03d_%(title)s_[%(id)s]_0000_%(duration)04d.%(ext)s"
 
 
